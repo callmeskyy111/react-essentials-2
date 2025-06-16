7c9852f93c9dbfd27f750a608d2d792a7415056b
Soumadip <39thdecember@gmail.com>
2025-06-16 22:21:46 +0530

    Updating States in NESTED ARRAYS[]

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,26 @@ const initialGameBoard = [
 ];
 
 function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIdx, colIdx) {
+    setGameBoard((prevBoard) => {
+      // temp. symbol - X
+      const updatedBoard = [...prevBoard.map(innerArray => [...innerArray])]; // nested structure.
+      updatedBoard[rowIdx][colIdx] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIdx) => (
+      {gameBoard.map((row, rowIdx) => (
         <li key={rowIdx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
               <li key={colIdx}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIdx, colIdx)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
